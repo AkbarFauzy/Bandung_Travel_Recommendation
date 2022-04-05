e83abb31a2c929b7f586fa1cac549a5044a83bd0
Moch. Nauval Rizaldi Nasril <mochnauvalrizaldinasril@gmail.com>
2022-04-05 10:30:06 +0700

    Update PlaceController.php
    add new API
    getTopHotels
    getTopDestinations

--- Bandung_Travel_Recommendation/app/Http/Controllers/API/PlaceController.php
+++ Bandung_Travel_Recommendation/app/Http/Controllers/API/PlaceController.php
@@ -39,12 +39,32 @@ class PlaceController extends Controller
 
     public function getTopHotels(Request $req){
         try {
-            $data = Place::whereRelation('place_types', 'name', '=', "Hotel")->get()
-            ->makeHidden(['type_place_id']);
+            $take = 5;
+            if($req->has('take')){
+                $take = $req->input('take');
+            }
+            $data = DB::table('places as p')
+            ->join(DB::raw("(SELECT place_id, COUNT(*) as view FROM user_interact_logs GROUP BY user_interact_logs.place_id) as i"), 'i.place_id', '=', 'p.id')
+            ->join('place_types as pt', 'pt.id', '=', 'p.type_place_id')
+            ->select(['p.*','i.view'])
+            ->where('pt.name', '=', "Hotel")
+            ->orderBy('view', 'DESC')
+            ->take($take)
+            ->get();
+
+            if($data->count() <= 5){
+                $data = Place::whereRelation('place_types', 'name', '=', "Hotel")
+                ->select('*')
+                ->addSelect(DB::raw("'0' as view"))
+                ->orderBy('rate', 'DESC')
+                ->take($take)
+                ->get();
+            }
+            
         } catch (\Exception $exception) {
             return response()->json([
                 'success' => false,
-                'message' => 'Get Hotels Failed!',
+                'message' => 'Get Top Hotels Failed!',
                 'errormsg' => $exception->getMessage(),
                 'data' => ''
             ], 404, $this->headers);
@@ -52,7 +72,7 @@ class PlaceController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => 'Get Hotels Success!',
+            'message' => 'Get Top Hotels Success!',
             'data' => $data
         ], 200, $this->headers);
     }
@@ -103,6 +123,47 @@ class PlaceController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Get Destination Types Success!',
+            'data' => $data
+        ], 200, $this->headers);
+    }
+
+    
+    public function getTopDestinations(Request $req){
+        try {
+            $take = 5;
+            if($req->has('take')){
+                $take = $req->input('take');
+            }
+            $data = DB::table('places as p')
+            ->join(DB::raw("(SELECT place_id, COUNT(*) as view FROM user_interact_logs GROUP BY user_interact_logs.place_id) as i"), 'i.place_id', '=', 'p.id')
+            ->join('place_types as pt', 'pt.id', '=', 'p.type_place_id')
+            ->select(['p.*','i.view'])
+            ->where('pt.name', '!=', "Hotel")
+            ->orderBy('view', 'DESC')
+            ->take($take)
+            ->get();
+
+            if($data->count() <= 5){
+                $data = Place::whereRelation('place_types', 'name', '!=', "Hotel")
+                ->select('*')
+                ->addSelect(DB::raw("'0' as view"))
+                ->orderBy('rate', 'DESC')
+                ->take($take)
+                ->get();
+            }
+            
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Get Top Destinations Failed!',
+                'errormsg' => $exception->getMessage(),
+                'data' => ''
+            ], 404, $this->headers);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get Top Destinations Success!',
             'data' => $data
         ], 200, $this->headers);
     }
