ca2382e1709e3bc37c66fc46bf3a925c77e0754f
AkbarFauzy <akbarfa625@gmail.com>
2022-04-11 22:28:06 +0700

    Admin Page and Datatables
    - Adding Admin Page layout
    - Adding UI For Destination Table
    - Importing bootbox ,sweetalert and yajra

--- Bandung_Travel_Recommendation/routes/web.php
+++ Bandung_Travel_Recommendation/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminDashboardController;
+use App\Http\Controllers\Backend\AdminDestinationController;
+use App\Http\Controllers\Backend\AdminUsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('/Frontend/landing-page');
 });
+
+Route::get('/admin/', [AdminDashboardController::class, 'view'])->name('admin.dashboard');
+Route::get('/admin/destination', [AdminDestinationController::class, 'view'])->name('admin.destination');
+Route::get('/admin/destination/form', [AdminDestinationController::class, 'loadForm'])->name('admin.destination.form');
+Route::post('/admin/destination/add', [AdminDestinationController::class, 'create'])->name('admin.destination.add');
+Route::get('/admin/destination/form/{id}', [AdminDestinationController::class, 'loadForm'])->name('admin.destination.form.edit');
+Route::get('/admin/type', [AdminDashboardController::class, 'view'])->name('admin.destinationtype');
+Route::get('/admin/users', [AdminUsersController::class, 'view'])->name('admin.users');
--- Bandung_Travel_Recommendation/vendor/composer/autoload_real.php
+++ Bandung_Travel_Recommendation/vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInit752df34cd91ee470c719718fded65d4f
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire752df34cd91ee470c719718fded65d4f($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
